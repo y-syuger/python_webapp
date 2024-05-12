a6a5d45f45b9e26da0813398ed6d6f54491f7f62
y-syuger <9201abcd@gmail.com>
2024-05-12 21:40:05 +0900

    [Add] todo

---
+++ todo.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template,request, redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, time
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    todo = db.Column(db.String(200), nullable=False)
+    deadline = db.Column(db.Date, nullable=False)
+    deadline_time = db.Column(db.String(20))
+
+#日付のみを入力された場合のデフォルト時刻
+DEFAULT_TIME = time(hour=12, minute=0)
+
+@app.route('/')
+def index():
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template('index.html', todo_list=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo = request.form['todo']
+    deadline = datetime.strptime(request.form['deadline'], '%Y-%m-%d')
+    deadline_time = request.form.get('deadline_time')
+    if not deadline_time:
+        deadline_time = DEFAULT_TIME.strftime('%H:%M')
+    print("Deadline Time:",deadline_time)
+    # データベースにToDo項目を追加
+    new_todo = Todo(todo=todo, deadline=deadline, deadline_time=deadline_time)
+    db.session.add(new_todo)
+    db.session.commit()
+    # todo_list.append({'todo':todo, 'deadline':deadline, 'deadline-time':deadline_time})
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>')
+def delete_todo(index):
+    todo_to_delete = Todo.query.get_or_404(index)
+    db.session.delete(todo_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
