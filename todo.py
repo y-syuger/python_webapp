5c62938a068a738d3b64cebe41e9169d17377bd0
y-syuger <9201abcd@gmail.com>
2024-06-15 18:58:59 +0900

    [Update] todo

--- todo.py
+++ todo.py
@@ -1,6 +1,9 @@
+import requests
+import atexit
 from flask import Flask, render_template,request, redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, time
+from datetime import datetime, time, date
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
@@ -11,22 +14,32 @@ class Todo(db.Model):
     todo = db.Column(db.String(200), nullable=False)
     deadline = db.Column(db.Date, nullable=False)
     deadline_time = db.Column(db.String(20))
+    is_completed = db.Column(db.Boolean, default=False)
 
-#日付のみを入力された場合のデフォルト時刻
+    def __repr__(self):
+        return f'<Todo {self.id}: {self.todo} -Deadline: {self.deadline}>'
+
+#日付・時間が空欄だった時のデフォルト値
+DEFAULT_DATE = str(date.today())
 DEFAULT_TIME = time(hour=12, minute=0)
 
 @app.route('/')
 def index():
     todo_list = Todo.query.all()
-    print(todo_list)
     return render_template('index.html', todo_list=todo_list)
 
 @app.route('/add', methods=['POST'])
 def add_todo():
-    todo = request.form.get('todo', '').sprit()
-    deadline = datetime.strptime(request.form.get('deadline', ''), '%Y-%m-%d')
+    todo = request.form.get('todo', '').split()
+    todo = ' '.join(todo)
 
-    if not todo or not deadline:
+    deadline_str = request.form.get('deadline', '')
+    if not deadline_str:
+        deadline_str = DEFAULT_DATE
+    
+    #deadline = datetime.strptime(request.form.get('deadline', ''), '%Y-%m-%d')
+    deadline = datetime.strptime(deadline_str, '%Y-%m-%d')
+    if not todo:
         return redirect(url_for('index', error="Todoと期限は必須項目です。"))
     
     deadline_time = request.form.get('deadline_time')
@@ -37,7 +50,6 @@ def add_todo():
     new_todo = Todo(todo=todo, deadline=deadline, deadline_time=deadline_time)
     db.session.add(new_todo)
     db.session.commit()
-    # todo_list.append({'todo':todo, 'deadline':deadline, 'deadline-time':deadline_time})
     return redirect(url_for('index'))
 
 @app.route('/edit/<int:index>', methods=['GET', 'POST'])
@@ -53,6 +65,13 @@ def edit_todo(index):
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('edit.html', todo=todo_to_edit)
+
+@app.route('/complete/<int:todo_id>', methods=['POST'])
+def complete_todo(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    todo.is_completed = not todo.is_completed
+    db.session.commit()
+    return redirect(url_for('index'))
 
 @app.route('/delete/<int:index>')
 def delete_todo(index):
